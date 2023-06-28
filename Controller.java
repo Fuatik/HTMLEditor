b64102cbbdd0093256552f0f97a9d63011be9710
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-06-28 16:49:09 +0100

    Controller.setPlainText

--- Controller.java
+++ Controller.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task32.task3209;
 
+import javax.swing.text.BadLocationException;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
 
 public class Controller {
 
@@ -46,5 +49,17 @@ public class Controller {
         document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
         document.addUndoableEditListener(view.getUndoListener());
         view.update();
+    }
+
+    public void setPlainText(String text) {
+        resetDocument();
+        StringReader reader = new StringReader(text);
+        try {
+            new HTMLEditorKit().read(reader, document, 0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (BadLocationException e) {
+            ExceptionHandler.log(e);
+        }
     }
 }
