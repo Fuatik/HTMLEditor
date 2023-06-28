dd621ff76107b5c85c555d734df654f6f5ad50d5
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-06-28 14:31:34 +0100

    init HTML Editor

---
+++ View.java
@@ -0,0 +1,65 @@
+package com.javarush.task.task32.task3209;
+
+import com.javarush.task.task32.task3209.listeners.FrameListener;
+import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class View extends JFrame implements ActionListener {
+
+    private Controller controller;
+    private JTabbedPane tabbedPane = new JTabbedPane(2);
+    private JTextPane htmlTextPane = new JTextPane();
+    private JEditorPane plainTextPane = new JEditorPane();
+
+    public Controller getController() {
+        return controller;
+    }
+
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public void init() {
+        initGui();
+        FrameListener listener = new FrameListener(this);
+        addWindowListener(listener);
+        setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
+
+    public void exit() {
+        controller.exit();
+    }
+
+    public void initMenuBar() {
+
+    }
+
+    public void initEditor() {
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.add("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.add("Текст", new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(null);
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
+    }
+
+    public void initGui() {
+        initMenuBar();
+        initEditor();
+        pack();
+    }
+
+    public void selectedTabChanged() {
+
+    }
+}
